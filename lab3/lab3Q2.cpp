6f964d845ba545099bb7d369ccf1aaf45e007b6a
GaneshN29 <77326035+GaneshN29@users.noreply.github.com>
2021-12-14 19:17:51 +0530

    Add files via upload

--- lab3/lab3Q2.cpp
+++ lab3/lab3Q2.cpp
@@ -1,67 +1,80 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class sparse{
+
+class Sparse{
     public:
-int a[10][3];
-void read(int a[10][3],int m);
-void print(int a[10][3],int m);
-void trans(int a[10][3],int m);
-};
-void sparse::read(int a[10][3],int m){
-int i,j,e,n,nonZ;
-cout<<"Enter the row,column and number of non-zero Element:\n";
-cin>>e>>n>>nonZ;
-a[0][0]=e;
-a[0][1]=n;
-a[0][2]=nonZ;
-cout<<"Enter the value of row,column and non-zero Element:\n";
-for(i=1; i<m; i++){
-  for(j=0; j<3; j++){
-    cin>>a[i][j];
-  }
-}
-}
-void sparse::trans(int a[10][3],int m){
-    int temp,i,j,k;
-    temp=a[0][0];
-    a[0][0]=a[0][1];
-    a[0][1]=temp;
-for(i=1; i<m; i++){
-    temp=a[i][0];
-    a[i][0]=a[i][1];
-    a[i][1]=temp;
-  }
-  for(i=1; i<m; i++){
-        for(j=i+1;j<m; j++){
-  if(a[i][0]>a[j][0]){
-    for(k=0; k<3; k++){
-        temp=a[i][k];
-    a[i][k]=a[j][k];
-    a[j][k]=temp;
+    int row;
+    int column;
+    int value;
+    void set(int r, int c, int v){
+        row=r;column=c;value=v;
     }
-  }
-  }}
-}
-void sparse::print(int a[10][3],int m){
-    int i,j;
-    cout<<"Row\tCol\tValue\n";
-for(i=0; i<m; i++){
-  for(j=0; j<3; j++){
-    cout<<a[i][j]<<"\t";
-  }
-  cout<<"\n";
-  }
-}
+    void FTranspose(Sparse a[], Sparse b[]){
+        int r=a[0].row;
+        int c=a[0].column;
+        int v=a[0].value;
+        int frequency[c]={0};
+        int start[c];
+        b[0].set(a[0].column,a[0].row,a[0].value);
+        if(v>0){
+            for(int i=1;i<=v;i++){
+                frequency[a[i].column]++;
+            }
+            start[0]=1;
+            for(int i=1;i<c;i++){
+                start[i]=start[i-1]+frequency[i-1];
+            }
+            for(int i=0;i<=v;i++){
+                int j=start[a[i].column]+1;
+                b[j++].set(a[i].column,a[i].row,a[i].value);
+            }
+        }
+    }
+    void display(){
+        cout<<row<<' '<<column<<' '<<value<<" "<<endl;
+    }
+
+};
+
+
 int main(){
-sparse g;
-int a[10][3],e;
-cout<<"Enter the number of non zero element:\n";
-cin>>e;
-e=e+1;
-g.read(a,e);
-cout<<"Sparse matrix before transpose:\n";
-g.print(a,e);
-g.trans(a,e);
-cout<<"Sparse matrix after transpose:\n";
-g.print(a,e);
+    //Accepting the input in the Square Matrix form
+    int m,n;
+    cout<<"Enter the size of the matrix:"<<endl;
+    cin>>m>>n;
+    int arr[m][n];
+    int count=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<"Enter ["<<i<<"]["<<j<<"]:";
+            cin>>arr[i][j];
+            if(arr[i][j]!=0)
+                count++;
+        }
+    }
+
+    //Developing a sparse matrix
+    Sparse s[count+1];
+    s[0].set(m,n,count);
+    int k=0;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]!=0){
+                s[++k].set(i,j,arr[i][j]);
+            }
+        }
+    }
+
+    //Original Matrix
+    cout<<"Matrix before Transposing"<<endl;
+    for(int k=0;k<=count;k++){
+        s[k].display();
+    }
+
+    Sparse b[count+1], temp;
+    cout<<"Matrix after Transposing"<<endl;
+    temp.FTranspose(s,b);
+    for(int k=0;k<=count;k++){
+        b[k].display();
+    }
 }
